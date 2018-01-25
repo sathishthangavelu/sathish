051c1d4e6fce3f4ba0b8f66c5585cdcebfbfed41
sathishthangavelu <35728112+sathishthangavelu@users.noreply.github.com>
2018-01-25 16:59:42 +0530

    Create sathish2.c

---
+++ sathish2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int x;
+    printf("entre the number x=");
+    scanf("%d",&x);
+    if(x%2==0)
+        printf("the number is even number");
+        else
+        printf("the numder is odd number");
+}
