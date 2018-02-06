5aa555000e6d8a4c51d0406a79348354d77c352c
sathishthangavelu <35728112+sathishthangavelu@users.noreply.github.com>
2018-02-06 16:11:33 +0530

    Create sathish5.c

---
+++ sathish5.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+void main()
+{
+    int a,b,c,x;
+    int largest;
+    printf("entre the number");
+    scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+printf("largest a");
+else if(b>a)
+printf("largest b");
+else(c>a);
+printf("the largest c");
+    }
