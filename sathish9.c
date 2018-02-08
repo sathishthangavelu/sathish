87959695f472c7c12e5b826d60e60e665a86d245
sathishthangavelu <35728112+sathishthangavelu@users.noreply.github.com>
2018-02-08 15:48:16 +0530

    Create sathish9.c

---
+++ sathish9.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+void main()
+{
+	int n,b,k,sum=0,c[100];
+	printf("Enter b value\n");
+	scanf("%d%d\n",&n,&k);
+	for(b=0;b<n;b++)
+	{
+            scanf("%d",&b[c]);
+	for(b=0;b<k;b++)
+	{
+		sum+=b[c];
+	}
+	}
+printf("%d",sum);
+
+}
+
+
