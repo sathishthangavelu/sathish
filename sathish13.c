275946aec39a3e74a5d26e320d68e2107a43ec75
sathishthangavelu <35728112+sathishthangavelu@users.noreply.github.com>
2018-02-09 15:59:57 +0530

    Create sathish13.c

---
+++ sathish13.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+void main()
+{
+    int n,i;
+    printf("enter n value");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("number not prime");
+    }
+    else
+    {
+        printf("number is prime");
+    }
+}
