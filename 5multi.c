0ba1b23cf0c8eb42ecc9d655b53a10e0d39bd61c
sathishthangavelu <35728112+sathishthangavelu@users.noreply.github.com>
2018-03-02 14:50:04 +0530

    Create 5multi.c

---
+++ 5multi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int a,i;
+    printf("enter the number");
+    scanf("%d",&a);
+    for(i=1;i<=5;i++)
+    {
+    printf("%d \n",a*i);
+    }
+    return 0;
+}
