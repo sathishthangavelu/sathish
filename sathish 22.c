4fa54f2afaa9dc713b47db5f01c262239bf26433
sathishthangavelu <35728112+sathishthangavelu@users.noreply.github.com>
2018-03-13 16:46:29 +0530

    Create sathish 22.c

---
+++ sathish 22.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[5],n,i;
+    printf("enter the array");
+    scanf("%d",&n);
+    printf("enter the value");
+    for(i=1;i<=n;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    if(a[1]>a[2]&&a[1]>a[3])
+    {
+        printf("%d",a[1]);
+    }
+    else if(a[2]>a[1]&&a[2]>a[3])
+    {
+        printf("%d",a[2]);
+    }
+    else(a[3]>a[1]&&a[3]>a[2]);
+    {
+        printf("%d",a[3]);
+    }
+    
+}
